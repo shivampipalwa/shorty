f10536064a717f1d62ea49c9a361ee88a0fc5494
Shivam <68528318+shivampipalwa@users.noreply.github.com>
2023-10-17 20:06:22 +0000

    Generate and save html for each url

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ const redirectsFile = fs.readFileSync(path.join(__dirname , 'redirects.yml') , '
 const redirects = YAML.parse(redirectsFile);
 
 // 2. generate an html page for each redirect url from template.html
+const templateHTML = fs.readFileSync(path.join(__dirname , 'template.html') , 'utf-8')
+for( let [slug, url] of Object.entries(redirects)){
+
+    console.log('Genrating HTML page for: ', slug)
+
+    const html = templateHTML.replace('https://example.com', url)
+    
+    //create folder for each slug
+    const folderPath = path.join(__dirname , 'out' , slug)
+    fs.mkdirSync(folderPath, {recursive: true})
+
+    fs.writeFileSync(path.join(folderPath, 'index.html') , html)
+
+}
