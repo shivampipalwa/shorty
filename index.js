ef0092fcef2bded9ba858e6843cca8ac1d22689e
Shivam <68528318+shivampipalwa@users.noreply.github.com>
2023-10-17 19:39:42 +0000

    Read redirects.yml and parse it

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // 1. load all the urls from redirects.yml
+const YAML = require('yaml');
+const fs = require('fs')
+const path = require('path')
+
+const redirectsFile = fs.readFileSync(path.join(__dirname , 'redirects.yml') , 'utf-8')
+const redirects = YAML.parse(redirectsFile);
 
 // 2. generate an html page for each redirect url from template.html
